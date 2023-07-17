d1c141fffae468b2eaebd26f94d5719d4c322175
Manas Rachh <mrachh@flatironinstitute.org>
2023-07-17 11:42:45 -0400

    updating rhs script

--- matlab/+mwscripts/update_rhs.m
+++ matlab/+mwscripts/update_rhs.m
@@ -2,7 +2,7 @@ opts_rhs = [];
 opts_rhs.itype = 2;
 opts_rhs.alpha = dir_radians;
 fprintf('Updating rhs\n');
-rhs = clm.get_rhs_gui_clm_cases(chnk_array, ...
+rhs = clm.get_rhs_gui(chnk_array, ...
     clmparams,clmparams.npts,clmparams.alpha1,clmparams.alpha2,opts_rhs);
 clear opts_rhs
 disp('updated rhs')
